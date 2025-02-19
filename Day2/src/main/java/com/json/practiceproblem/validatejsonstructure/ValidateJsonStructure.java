629b8d7490bef8684653cddc8fba69c16a6a31c5
srijan0412 <srijan.soni01@gmail.com>
2025-02-19 12:58:47 +0530

    [Srijan] Add . Validate Json Structure

--- Day2/src/main/java/com/json/practiceproblem/validatejsonstructure/ValidateJsonStructure.java
+++ Day2/src/main/java/com/json/practiceproblem/validatejsonstructure/ValidateJsonStructure.java
@@ -1,4 +1,23 @@
 package com.json.practiceproblem.validatejsonstructure;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.File;
+import java.io.IOException;
+
 public class ValidateJsonStructure {
+    public static void main(String[] args) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        File jsonFile = new File("S:\\Capgemini\\Week-5\\Day2\\src\\main\\java\\com\\json\\practiceproblem\\validatejsonstructure\\Sample.json");
+
+        try {
+            // Parsing JSON file to check if it's valid
+            JsonNode jsonNode = objectMapper.readTree(jsonFile);
+            System.out.println("Valid Json Structure:\n" + jsonNode.toPrettyString());
+            System.out.println(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(jsonNode));
+        }
+        catch(IOException e) {
+            System.out.println("Invalid Json: " + e.getMessage());
+        }
+    }
 }
